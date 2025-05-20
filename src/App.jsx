fd642d3c7656ad5eff4542d140b59140be4714e6
Leonel-Lara <leonel.silva.14@hotmail.com>
2025-05-20 18:10:33 -0300

    🥳 Init project

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <main>
+      <h1 className="text-5xl text-violet-300 font-bold">
+        Welcome to zentry project
+      </h1>
+    </main>
+  );
+};
+
+export default App;
