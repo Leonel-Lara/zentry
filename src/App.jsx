4d15f90a23748d976426a19155b79fbc35f1a755
Leonel-Lara <leonel.silva.14@hotmail.com>
2025-05-29 19:06:28 -0300

    ✅ about section

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import Navbar from "./components/Navbar.jsx";
 import Hero from "./components/Hero.jsx";
 import About from "./components/About.jsx";
 
 const App = () => {
   return (
     <main className="relative min-h-screen w-screen overflow-x-hidden">
+      <Navbar />
       <Hero />
       <About />
     </main>
